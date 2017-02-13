c7b4ee0e79e78cbdd10ed313121b4f57a59037ef
EVE DENISON <evedenison@gmail.com>
2017-02-13 11:52:42 -0800

    begin handlebars template conversion

--- scripts/projects.js
+++ scripts/projects.js
@@ -12,17 +12,12 @@ function Project (opts) {
 
 Project.prototype.toHtml = function () {
 
-  var $newProject = $('.template').clone();
-  $newProject.removeClass('template');
+  var template = Handlebars.compile($('#article-template').html());
 
-  $newProject.find('a').attr('href', this.projectUrl);
-  $newProject.find('h1:first').text(this.title);
-  $newProject.find('.project-body').html(this.body);
-  $newProject.find('time[pubdate]').attr('datetime', this.publishedOn);
-  $newProject.find('time').text('about ' + parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000) + ' days ago');
-  $newProject.append('<hr>');
-  console.log($newProject.html());
-  return $newProject;
+  this.daysAgo = parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000);
+  this.publishStatus = this.publishedOn ? `published ${this.daysAgo} days ago` : '(draft)';
+
+  return template(this);
 };
 
 sourceData.forEach(function(ele) {
@@ -31,5 +26,5 @@ sourceData.forEach(function(ele) {
 // console.log('sourceData ' + sourceData);
 
 allProjects.forEach(function(a){
-  $('#project').append(a.toHtml());
+  $('#projects').append(a.toHtml());
 });
