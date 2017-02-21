ab817acce355739b465077ace1e4e3b7d5a82942
EVE DENISON <evedenison@gmail.com>
2017-02-21 11:29:50 -0800

    added closures and replaced for loop

--- public/scripts/projectsView.js
+++ public/scripts/projectsView.js
@@ -1,19 +1,21 @@
 'use strict';
+(function(module){
+  var projectsView = {}
 
-var projectsView = {}
-
-projectsView.handleNavBar = function () {
-  $('.main-nav-bar').on('click', '.tab', function() {
-    $('.nav-content').hide();
-    $('#' + $(this).data('content')).fadeIn();
-  });
-  $('.main-nav-bar .tab:first').click();
-};
+  projectsView.handleNavBar = function () {
+    $('.main-nav-bar').on('click', '.tab', function() {
+      $('.nav-content').hide();
+      $('#' + $(this).data('content')).fadeIn();
+    });
+    $('.main-nav-bar .tab:first').click();
+  };
 
 
-projectsView.initProjectPage = function (){
-  Project.all.forEach(function (a){
-    $('#projects').append(a.toHtml())
-  });
-  projectsView.handleNavBar();
-}
+  projectsView.initProjectPage = function (){
+    Project.all.forEach(function (a){
+      $('#projects').append(a.toHtml())
+    });
+    projectsView.handleNavBar();
+  }
+  module.projectsView = projectsView;
+})(window);
